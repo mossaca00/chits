624a5e1a7c55516cfe05b4a11548a788474dd9d0
macmac617 <mossaca00@gmail.com>
2015-02-17 11:16:25 +0800

    modal

--- application/views/patientSearch/imports/view_labs.php
+++ application/views/patientSearch/imports/view_labs.php
@@ -4,6 +4,30 @@
 		<center><strong><h4>LABORATORY REQUESTS</h4></strong></center>
 		<h3><span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span></h3>
 		<p><a href="#" type="button" class="btn btn-default"> Refresh </a></p>
+		<fieldset>
+			<legend>LABORATORY REQUEST FORMS</legend>
+			<p><a href="#">General Laboratory Request Form</a></p>
+			<p><a href="#">Direct Sputum Smear Microscopy</a></p>
+			<p><a href="#">Complete Blood Count</a></p>
+			<p><a href="#">Fasting Blood Sugar</a></p>
+			<p><a href="#">Fecalysis</a></p>
+			<p><a href="#">Lipid Profile</a></p>
+			<p><a href="#">Urinalysis</a></p>
+			<p><a href="#">XRay</a></p>
+			<p><a href="#">Hepatitis B Profile</a></p>
+			<p><a href="#">Ultrasound</a></p>
+			<p><a href="#">Serology</a></p>
+			<p><a href="#">RPR</a></p>
+			<p><a href="#">VDRL</a></p>
+			<p><a href="#">Gram Stain</a></p>
+			<p><a href="#">Wet Mount</a></p>
+			<p><a href="#">KOH Smear</a></p>
+			<p><a href="#">HIV Testing (need to fill out DOH-NEC form A)</a></p>
+			<p><a href="#">Skin Smear</a></p>
+			<p><a href="#">Pregnancy Test</a></p>
+			<p><a href="#">TPHA / TPPA</a></p>
+		</fieldset>
+
 	</div>
 	<div class="col-md-6">
 
@@ -18,11 +42,10 @@
 				<label><input type="checkbox">by Service</label>
 				</p>
 			</div>
-			<div class="form-group">
+			<div class="form-group" style="margin-left:85px;">
 				<label>Search:<input type="text" class="form-inline"></label>
 			</div>
 		</form>
-		<br><br>
 		<p><h3><span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span></h3></p>
 		<p><a href="#" type="button" class="btn btn-default"> Refresh </a></p>
 
--- application/views/queries/view_queries.php
+++ application/views/queries/view_queries.php
@@ -5,7 +5,7 @@
 	
 	<div class="row">
 		<div class="col-md-12">
-			<h2> <strong>QUERIES</strong> </h2>
+			<h2> <strong>Health Center Reports</strong> </h2>
 		</div>
 		<div class="col-md-12">
 			<ul class="nav nav-tabs">
@@ -15,7 +15,7 @@
 		<div class="col-md-12">
 			<br><h4><strong>Summary Raports</strong></h4>
 			<ol type="1">
-				<li><h5><a href="#">Daily Service Reports</a></h5></li>
+				<li><h5><a href="#report" role="button" data-toggle="modal">Daily Service Reports</a></h5></li>
 				<li><h5><a href="#">Newly Registered Patients</a></h5></li>
 			</ol>
 			<br><h4><strong>Registries</strong></h4>
@@ -29,6 +29,42 @@
 						<li style="display:block;"><h5><a href="#"><span class="glyphicon glyphicon-arrow-right"></span>Master list of Families</a></h5></li>
 					</ul>
 			</ol>			
+		</div>
+	</div>
+
+	<div class="modal fade" id="report">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h3>General Daily Service Report</h3>
+				</div>
+				<form action="">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12">
+								<p><h4><strong>Daily Service Report</strong></h4></p>
+								<p><h4 style="color:#1F8BB2;">INCLUSIVE REPORT FOR THIS DATE</h4></p>
+								<br><br>
+							</div>
+							<div class="col-md-6">
+								
+									<div class="form-group">
+										<label>Start Date: </label> <input type="text" placeholder="00/00/00">
+										<cite><small>(Format: mm/dd/yyyy)</small></cite>									
+									</div>
+									<div class="form-group">
+										<label>End Date: </label> <input type="text" placeholder="00/00/00">
+										<cite><small>(Format: mm/dd/yyyy)</small></cite>
+									</div>
+							</div>
+						</div>
+					</div>
+					<div class="modal-footer">
+						<button type="submit" class="btn btn-danger" data-dismiss="modal">Close <span class="glyphicon glyphicon-remove"></span></button>
+						<button type="submit" class="btn btn-success" data-dismiss="modal">Generate <span class="glyphicon glyphicon-ok-circle"></span></button>
+					</div>
+				</form>
+			</div>
 		</div>
 	</div>
 
